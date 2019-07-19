f289255bdea774c2e85e40ebcf5bd44f2fae576f
CS-Schmidt <52944232+CS-Schmidt@users.noreply.github.com>
2019-07-19 17:47:04 -0600

    Add files via upload
    Demo1 files worked on by Ethan, Alex, and Edrick.

---
+++ Demo1Runner.java
@@ -0,0 +1,34 @@
+package demo1;
+
+//Method to test validity grid
+/*for (int i = 0; i < k.length;i++) {
+	for (int j = 0; j < k[0].length; j++) {
+		System.out.print(k[i][j]+ " ");
+	}
+	System.out.println("\n");
+}
+*/
+
+//COMPONENTS
+
+//1) Game starts and we are prompted to place a tower.
+	//a) If the player places a tower on the road they are re-prompted to place it again.
+
+//2) Once tower placement is initialized the player is prompted that the wave is starting.
+
+//3) Enemy spawns and moves sequentially across the road on a timer.
+	//a)If the enemy enters the tower objects range it takes damage.
+	//b)If the enemy passes the tower we take damage when it reaches the end.
+
+public class Demo1Runner {
+	public static void main(String[]args) {
+		TdMap map = new TdMap(5,3);
+		
+
+		}
+	}
+
+
+	
+
+
