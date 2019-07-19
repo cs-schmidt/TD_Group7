6368ef6d4830c91f61832abf10eeb617830c876f
EddieLin1 <52938427+EddieLin1@users.noreply.github.com>
2019-07-19 17:44:44 -0600

    enemy class

---
+++ Enemy.java
@@ -0,0 +1,21 @@
+
+class Enemy{
+	
+private int health;
+private int damage;
+
+
+	public Enemy(int health, int damage) {
+		this.health = health;
+		this.damage = damage;
+		
+	}
+	public int get_health() {
+		return health;
+	}
+	public int get_damage() {
+		return damage;
+	}
+	
+
+}
