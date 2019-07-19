e6625b8456d613b2420898c73cda9b22f38151f6
Alexhanzx <53067070+Alexhanzx@users.noreply.github.com>
2019-07-19 17:48:07 -0600

    Add files via upload
    Towers

---
+++ Tower.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Tower extends TdMap{
+	
+	private int damage;
+	private int range;
+	//TODO: private int attack_speed;
+	
+	public Tower(int tower_damage, int tower_range) {
+		damage = tower_damage;
+		range = tower_range;
+	}
+	
+	public boolean check_enemy() {
+		
+	}
+	
+	public void priority() {
+		
+	}
+	
+	public int damage() {
+		
+		
+	}
+
+}
